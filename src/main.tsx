f882beea3a1e5b6d56ec06268e682754aca28d5b
Avis Ebron <avisebron@gmail.com>
2024-04-24 05:24:41 -0500

    deployed website

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Home'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'; // Import the Router component
+import { BrowserRouter, Route, BrowserRouter as Router, Routes } from 'react-router-dom'; // Import the Router component
 import './index.css'
 import NavbarComponent from './Navbar';
 import AboutMe from './AboutMe';
@@ -9,13 +9,13 @@ import AboutMe from './AboutMe';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   
  <React.StrictMode>
-    <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutMe />} />
       </Routes>
       <NavbarComponent />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>,
 )
 

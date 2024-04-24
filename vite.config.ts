39d964187ac419a52358dbda5694bb1362da8dc1
Avis Ebron <avisebron@gmail.com>
2024-04-24 05:02:49 -0500

    deployed website

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
-import { defineConfig } from 'vite';
-import path from 'node:path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [require('@vitejs/plugin-react')],
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-    ],
-  },
-  build: {
-    outDir: 'build',
-    assetsDir: 'static',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        aboutMe: path.resolve(__dirname, 'AboutMe.tsx'),
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
